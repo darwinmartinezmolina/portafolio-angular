1cdb76fc50de4e9a32737d5628fe2d94030abd88
Darwin Martinez <darwin.martinezmolina@gmail.com>
2020-08-13 22:43:21 -0400

    End section 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,18 +9,35 @@ export class InfoPaginaService {
 
   info: InfoPagina = {}
   cargada = false;
+  equipo: any[] = [];
 
   constructor(private http: HttpClient) {
 
     // console.log('Servicio de infoPagina listo');
+    this.cargarInfo();
+    this.cargarEquipo();
 
+  }
+
+
+  private cargarInfo() {
     // Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
       .subscribe((resp: InfoPagina) => {
 
         this.cargada = true;
         this.info = resp;
-        console.log(resp);
+        // console.log(resp);
+      });
+  }
+
+  private cargarEquipo() {
+
+    this.http.get('https://angular-html-39fc3.firebaseio.com/equipo.json')
+      .subscribe((resp: any[]) => {
+
+        this.equipo = resp;
+        // console.log(resp);
       });
   }
 }
